32d61db2bbd4db1b3e332d40ae6741c9fcf7cfa2
Gabor Simon <gabor.simon75@gmail.com>
2024-06-05 20:25:43 +0400

    chain is indeed a chain now

--- pdfquery.py
+++ pdfquery.py
@@ -9,7 +9,7 @@ from langchain_community.vectorstores.chroma import Chroma
 from langchain_community.document_loaders import PyPDFium2Loader
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain_community.llms import OpenAIChat
-from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables import RunnableLambda, RunnableConfig
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from pydantic import BaseModel, Field
 from streamlit.runtime.uploaded_file_manager import UploadedFile
@@ -20,34 +20,40 @@ class DocumentTag(BaseModel):
     orig_source: str = Field(description="The source where the content came from")
 
 
-document_prompt = PromptTemplate(
-    input_variables=['page_content'],
-    output_parser=None,
-    partial_variables={},
-    template='{page_content}',
-    template_format='f-string',
-    validate_template=True
-)
-
-def stuff_whatever(input):
-    docs = input["input_documents"]
-    doc_strings = [format_document(doc, document_prompt) for doc in docs]
-    result = "\n\n".join(doc_strings)
-    return {
-        "context": result,
-        "question": input["question"]
-    }
-
 class PDFQuery:
     def __init__(self, openai_api_key = None) -> None:
-        self.embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         os.environ["OPENAI_API_KEY"] = openai_api_key
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        # self.llm = OpenAIChat(temperature=0, openai_api_key=openai_api_key) # .with_structured_output(DocumentTag)
-        self.llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
-        # self.structured_llm = self.llm.with_structured_output(DocumentTag) # FIXME: needs package upgrade to recent
 
-        _prompt = ChatPromptTemplate(
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+        self.vectordb = Chroma(embedding_function=embeddings, persist_directory="./chroma_db")
+        db_retriever = self.vectordb.as_retriever()
+
+        document_prompt = PromptTemplate(
+            input_variables=["page_content"],
+            output_parser=None,
+            partial_variables={},
+            template="{page_content}",
+            template_format="f-string",
+            validate_template=True
+        )
+
+        def f_stuff_documents(docs, config: RunnableConfig):
+            """
+            StuffDocumentsChain is not a chainable Runnable, so now we do it manually,
+            by wrapping this function with a RunnableLambda
+            """
+            return {
+                "question": config["metadata"]["question"],
+                "context": "\n\n".join(
+                    [ format_document(doc, document_prompt) for doc in docs ]
+                )
+            }
+
+        stuff_documents = RunnableLambda(f_stuff_documents)
+
+        prompt = ChatPromptTemplate(
             input_variables=["question", "context"],
             output_parser=None,
             partial_variables={},
@@ -81,35 +87,23 @@ If you don"t know the answer, just say that you don"t know, don"t try to make up
             ]
         )
 
-#        llm_chain = LLMChain(
-#            llm=self.llm,
-#            prompt=_prompt,
-#            verbose=None,
-#            callback_manager=None
-#        )
-#        self.chain = StuffDocumentsChain(
-#            llm_chain=llm_chain,
-#            document_variable_name="context",
-#            verbose=None,
-#            callback_manager=None
-#        )
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
+        # structured_llm = self.llm.with_structured_output(DocumentTag) # FIXME: needs package upgrade to recent
 
-        self.chain = RunnableLambda(stuff_whatever) | _prompt | self.llm
-
-        self.vectordb = Chroma(embedding_function=self.embeddings, persist_directory="./chroma_db")
-        self.db = self.vectordb.as_retriever()
-
-    def dump_vectordb(self) -> None:
-        print("Dumping the DB:")
-        print(self.vectordb.get())
+        self.chain = (
+            db_retriever |
+            stuff_documents |
+            prompt |
+            llm
+        )
 
     def ask(self, question: str) -> str:
-        docs = self.db.invoke(question)
-
         response = self.chain.invoke(
+            question,
             {
-                "input_documents": docs,
-                "question": question
+                "metadata": {
+                    "question": question
+                }
             }
         )
         return response.content
@@ -125,5 +119,4 @@ If you don"t know the answer, just say that you don"t know, don"t try to make up
         # self.vectordb.persist()  # deprecated
 
     def forget(self) -> None:
-        self.db = None
         self.chain = None
