d1434cb09531a6a8682065d6b2c2bee44ced2783
Gabor Simon <gabor.simon75@gmail.com>
2024-06-26 14:13:57 +0400

    vectorstore moved to Pinecone, hybrid search works

--- pdfquery.py
+++ pdfquery.py
@@ -1,33 +1,62 @@
 import os
-from typing import Any
+from typing import Any, List, Optional
 
-# from chromadb.errors import NoIndexException
 from langchain_core.prompts import PromptTemplate, format_document
 from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores.chroma import Chroma
+# from langchain_community.vectorstores.chroma import Chroma
+from langchain_core.retrievers import BaseRetriever
+from langchain_pinecone import PineconeVectorStore
+# from langchain_community.retrievers.pinecone_hybrid_search import PineconeHybridSearchRetriever   # FIXME: no good, it doesn't support filter=...
 from langchain_community.document_loaders import PyPDFium2Loader
-# from langchain_community.llms import OpenAIChat
-from langchain_core.runnables import RunnableLambda, RunnableConfig
+from langchain_core.runnables import RunnableLambda, RunnableConfig, RunnableSequence
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from pydantic import BaseModel, Field
+# from pydantic import BaseModel, Field
 from streamlit.runtime.uploaded_file_manager import UploadedFile
+from pinecone import Pinecone, Index as PineconeIndex
+from langchain_core.documents import Document
+from langchain_core.embeddings import Embeddings
 
 
-class DocumentTag(BaseModel):
-    page_content: str = Field(description="The content")
-    orig_source: str = Field(description="The source where the content came from")
+class PineconeReallyHybridSearchRetriever(BaseRetriever):
+    vectorstore: PineconeVectorStore
+
+    def __init__(self, vectorstore: PineconeVectorStore):
+        super().__init__(vectorstore=vectorstore)
+
+    def _get_relevant_documents(self, query: str, retriever_config: Optional[RunnableConfig], **kwargs) -> List[Document]:
+        qfilter = {
+            "source_type": {
+                "$eq": "application/pdf"
+            }
+        }
+
+        return self.vectorstore.similarity_search(
+            query=query,
+            filter=qfilter,
+            k=4
+        )
 
 
 class PDFQuery:
-    def __init__(self, openai_api_key=None) -> None:
-        os.environ["OPENAI_API_KEY"] = openai_api_key
+    vectorstore: PineconeVectorStore
+    chain: RunnableSequence
 
+    def __init__(self) -> None:
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        self.vectordb = Chroma(embedding_function=embeddings, persist_directory="./chroma_db")
-        db_retriever = self.vectordb.as_retriever()
+        embeddings: Embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
+
+        # self.vectorstore = Chroma(embedding_function=embeddings, persist_directory="./chroma_db")
+        # db_retriever = self.vectorstore.as_retriever()
+
+        client: Pinecone = Pinecone(api_key=os.environ["PINECONE_API_KEY"], source_tag="langchain")
+        index: PineconeIndex = client.Index("test-idx")
+        self.vectorstore = PineconeVectorStore(
+            embedding=embeddings,
+            index=index
+        )
+        db_retriever = PineconeReallyHybridSearchRetriever(vectorstore=self.vectorstore)
 
         document_prompt = PromptTemplate(
             input_variables=["page_content", "metadata"],
@@ -90,8 +119,7 @@ If you don't know the answer, just say that you don't know, don't try to make up
             ]
         )
 
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
-        # structured_llm = self.llm.with_structured_output(DocumentTag) # FIXME: needs package upgrade to recent
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=os.environ["OPENAI_API_KEY"])
 
         self.chain = (
             db_retriever |
@@ -101,13 +129,27 @@ If you don't know the answer, just say that you don't know, don't try to make up
         )
 
     def ask(self, question: str) -> str:
-        response = self.chain.invoke(
-            question,
-            {
-                "metadata": {
-                    "question": question
+        config: RunnableConfig = RunnableConfig(
+            tags=[],
+            metadata={
+                "question": question,
+                "foo": {
+                    "bar": "baz"
                 }
-            }
+            },
+            callbacks=None,
+            # configurable={
+            #     # this would be stuffed into metadata, but it doesn't support dicts
+            #     # so it's easier to just provide metadata above
+            #
+            #     # supported value types: str, int, float, bool
+            #     "question": question
+            # },
+        )
+        response = self.chain.invoke(
+            input=question,
+            config=config,
+            retriever_config=config  # NOTE: config isn't passed to _get_relevant_documents otherwise
         )
         return response.content
 
@@ -118,8 +160,8 @@ If you don't know the answer, just say that you don't know, don't try to make up
             doc.metadata["source_name"] = file.name
             doc.metadata["source_type"] = file.type
         split_documents = self.text_splitter.split_documents(documents)
-        self.vectordb.add_documents(split_documents)
-        # self.vectordb.persist()  # deprecated
+        self.vectorstore.add_documents(split_documents)
+        # self.vectorstore.persist()  # deprecated
 
     def forget(self) -> None:
         self.chain = None
--- streamlitui.py
+++ streamlitui.py
@@ -39,32 +39,12 @@ def read_and_save_file():
         os.remove(file_path)
 
 
-def is_openai_api_key_set() -> bool:
-    return len(st.session_state["OPENAI_API_KEY"]) > 0
-
-
 def main():
     if len(st.session_state) == 0:
         st.session_state["messages"] = []
-        st.session_state["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
-        if is_openai_api_key_set():
-            st.session_state["pdfquery"] = PDFQuery(st.session_state["OPENAI_API_KEY"])
-        else:
-            st.session_state["pdfquery"] = None
+        st.session_state["pdfquery"] = PDFQuery()
 
     st.header("ChatPDF")
-
-    if st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password"):
-        if (
-            len(st.session_state["input_OPENAI_API_KEY"]) > 0
-            and st.session_state["input_OPENAI_API_KEY"] != st.session_state["OPENAI_API_KEY"]
-        ):
-            st.session_state["OPENAI_API_KEY"] = st.session_state["input_OPENAI_API_KEY"]
-            if st.session_state["pdfquery"] is not None:
-                st.warning("Please, upload the files again.")
-            st.session_state["messages"] = []
-            st.session_state["user_input"] = ""
-            st.session_state["pdfquery"] = PDFQuery(st.session_state["OPENAI_API_KEY"])
 
     st.subheader("Upload a document")
     st.file_uploader(
@@ -74,13 +54,13 @@ def main():
         on_change=read_and_save_file,
         label_visibility="collapsed",
         accept_multiple_files=True,
-        disabled=not is_openai_api_key_set(),
+        disabled=False,
     )
 
     st.session_state["ingestion_spinner"] = st.empty()
 
     display_messages()
-    st.text_input("Message", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
+    st.text_input("Message", key="user_input", disabled=False, on_change=process_input)
 
     st.divider()
     st.markdown("Source code: [Github](https://github.com/Anil-matcha/ChatPDF)")
